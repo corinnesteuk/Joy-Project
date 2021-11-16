d9b3d69950c9ee26f460e8ea379f79f98276d737
Rachel Gordon <rachelgordon@Rachels-MacBook-Air.local>
2021-11-16 12:32:03 -0600

    Add Sept 2019 Data

--- JSON Files.py
+++ JSON Files.py
@@ -1,18 +1,41 @@
+
 import pandas as pd
 import json
+from datetime import datetime
 
 
-j = pd.read_json('tweets-2019-09-02.json', lines = True)
-j = j.drop(columns = ['_type', 'url', 'cashtags'])
-#j = j.set_index(['id'])
-print(j)
+def combine_month_2019(month, num_days):
 
-f2data = "" 
+    start_date = '2019-' + str(month) + '-02'
+    end_date = '2019-' + str(month) + '-' + str(num_days)
 
-with open('tweets-2019-09-01.json') as f2: 
-    f2data = '\n' + f2.read()
+    dates = pd.date_range(start = start_date, end = end_date)
+
+    for date in dates:
+
+        date = datetime.strftime(date, '%Y-%m-%d')
+        read_file = 'Data/tweets-' + str(date) + '.json'
+
+        print(date)
+
+        '''
+        j = pd.read_json(read_file, lines = True)
+        j = j.drop(columns = ['_type', 'url', 'cashtags'])
+        #j = j.set_index(['id'])
+        print(j)
+        '''
     
-with open('tweets-2019-09-02.json','a+') as f1:
-    f1.write(f2data)
-    j2 = pd.read_json('tweets-2019-09-02.json', lines = True)
-print(j2)
+        f2data = "" 
+
+        with open(read_file) as f2: 
+            f2data = '\n' + f2.read()
+    
+        with open('Data/tweets-2019-09-01.json','a+') as f1:
+            f1.write(f2data)
+
+
+    j2 = pd.read_json('Data/tweets-2019-09-01.json', lines = True)
+    print(j2)
+
+
+print(combine_month_2019(month = '09', num_days = 30))
