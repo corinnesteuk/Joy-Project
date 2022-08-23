6151a69f643dd326589e1d48fca5a179cb7af7c4
Rachel Gordon <rachelgordon@Rachels-MacBook-Air.local>
2022-08-23 16:14:58 -0500

    Adding comments

--- DataProcessing/AddJoyFlag.py
+++ DataProcessing/AddJoyFlag.py
@@ -1,53 +1,70 @@
+## Add a column to the data to indicate if a tweet includes the word joy
+
 import pandas as pd
 import json
 import nltk
 
 
-# add a column to the dataframe to indicate if a tweet includes the word joy
+# create a list of months to loop through month files
+dates = pd.date_range(start = '09/01/2019', end = '01/31/2022', freq = 'M')
 
-# months = []
-# for date in dates:
-#     date = str(date).split('-')
-#     year = date[0]
-#     month = date[1]
-#     months.append([month, year])
+months = []
+for date in dates:
+    date = str(date).split('-')
+    year = date[0]
+    month = date[1]
+    months.append([month, year])
 
-    # for m in months:
-    #file = 'tweets-' + str(month[0]) + '-' + str(month[1]) + '.json'
-m = ['11', '2021']
-file = '/Users/corinnesteuk/Downloads/tweets-11-2021.json'
-with open(file, 'r') as f:
-    data = json.load(f)
 
-for t in data['tweets']:
-    myElementDel = ('_type','url', 'conversationId', 'lang', 'source', 'sourceUrl', 'sourceLabel',
+
+# loop through month files
+## month files that were used are too large to add to GitHub
+for m in months:
+    file = 'tweets-' + str(month[0]) + '-' + str(month[1]) + '.json'
+
+    with open(file, 'r') as f:
+        data = json.load(f)
+
+
+    # drop irrelevant columns
+    for t in data['tweets']:
+        myElementDel = ('_type','url', 'conversationId', 'lang', 'source', 'sourceUrl', 'sourceLabel',
          'outlinks', 'tcooutlinks', 'media', 'retweetedTweet', 'renderedContent', 'quotedTweet',
          'inReplyToTweetId', 'inReplyToUser', 'mentionedUsers', 'cashtags')
-    for d in myElementDel:
-        t.pop(d)
-    t['user'] = t['user']['displayname']
-    t['place'] = t['place']['fullName']
-    t['coordinates'] = [t['coordinates']['longitude'], t['coordinates']['latitude']]
-df = pd.DataFrame(data['tweets'])
+        for d in myElementDel:
+            t.pop(d)
+        t['user'] = t['user']['displayname']
+        t['place'] = t['place']['fullName']
+        t['coordinates'] = [t['coordinates']['longitude'], t['coordinates']['latitude']]
+    df = pd.DataFrame(data['tweets'])
 
-new_tweets = []
-ijoy = []
+    new_tweets = []
+    ijoy = []
 
-for tweet in df['content']:
-    tweet = tweet.lower()
-    tokenizer = nltk.RegexpTokenizer(r"\w+")
-    words = tokenizer.tokenize(tweet)
 
-    if 'joy' in words:
-        # create an indicator variable ijoy (if joy is in the tweet)
-        ijoy.append(1)
+    # find tweets with the word "joy" in them and create ijoy column
+    for tweet in df['content']:
+        tweet = tweet.lower()
+        tokenizer = nltk.RegexpTokenizer(r"\w+")
+        words = tokenizer.tokenize(tweet)
 
-    else:
-        ijoy.append(0)
+        if 'joy' in words:
+            # create an indicator variable ijoy (if joy is in the tweet)
+            ijoy.append(1)
 
-    tweet = ' '.join(words)
-    new_tweets.append(tweet)
+        else:
+            ijoy.append(0)
 
-df['ijoy'] = ijoy
-df_joy = df.query('ijoy == 1')
-df_joy.to_csv(str(m[0]) + '-' + str(m[1]) + '.csv', mode = 'w' )
+        tweet = ' '.join(words)
+        new_tweets.append(tweet)
+
+
+
+    # create subsets of dataset based on ijoy value
+    df['ijoy'] = ijoy
+    df_joy = df.query('ijoy == 1')
+
+    ## find code to create non joy sample
+    #df_nojoy = df.query('ijoy == 0')
+    df_joy.to_csv('Joy-' + str(m[0]) + '-' + str(m[1]) + '.csv', mode = 'w' )
+    #df_nojoy.to_csv('NoJoy-' + str(m[0]) + '-' + str(m[1]) + '.csv', mode = 'w' )
--- DataProcessing/CombineJSONMonths.py
+++ DataProcessing/CombineJSONMonths.py
@@ -1,5 +1,5 @@
 
-# combine all files for one month in a given year
+# Combine all files for one month in a given year
 # run separately for each month in each year and enter number of days
 
 import pandas as pd
@@ -7,7 +7,8 @@ import json
 from datetime import datetime
 
 
-# combine month files for one year into one
+# combine day files for one month into a single month file
+## month files were too big to upload to GitHub
 def combine_month(year, month, num_days):
 
     first_file = 'L:\Joy/Data/tweets-' + str(year) + '-' + str(month) + '-01.json'
@@ -33,66 +34,10 @@ def combine_month(year, month, num_days):
             f1.write(f2data)
 
     # print the contents of the new total month file
-    #j2 = pd.read_json(first_file, lines = True)
-    #print(j2)
+    j2 = pd.read_json(first_file, lines = True)
+    print(j2)
 
 
 # call combine months function for a specific month
-print(combine_month(year = 2022, month = '01', num_days = 25))
-
-
-
-# combine month files from one year into one
-# give the desired year and the beginning and ending month for our data from that year
-
-def combine_year(start_month, end_month, year):
-
-    first_file = 'L:\Joy/Data/tweets-' + str(start_month) + '-' + str(year) + '.json'
-
-    for month in range(int(start_month) + 1, int(end_month) + 1):
-
-        if len(str(month)) == 1:
-            month = '0' + str(month)
-    
-        read_file = 'L:\Joy/Data/tweets-' + str(month) + '-' + str(year) + '.json'
-
-        print(read_file)
-    
-        f2data = "" 
-
-        with open(read_file) as f2: 
-            f2data = '\n' + f2.read()
-    
-        with open(first_file,'a+') as f1:
-            f1.write(f2data)
-
-
-# create combined file for 2021
-#print(combine_year(start_month = '01', end_month = '09', year = 2021))
-
-# file sizes
-# 09/2019: 1.4 mil KB
-# 01/2020: 1.454 mil KB
-# 01/2021: 1.156 mil KB
-
-
-
-'''
-# combine year files into one
-# files are too large so takes too much memory on current computer
-years = ['2020', '2021']
-
-for year in years:
-    
-    read_file = 'L:\Joy/Data/tweets-' + year + '.json'
-
-    print(read_file)
-    
-    f2data = "" 
-
-    with open(read_file) as f2: 
-        f2data = '\n' + f2.read()
-    
-    with open('L:\Joy/Data/tweets-2019.json','a+') as f1:
-        f1.write(f2data)
-'''
+## change parameters to match specific month
+print(combine_month(year = 2022, month = '01', num_days = 31))
